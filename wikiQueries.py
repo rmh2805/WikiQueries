c4efff039efec246dc2d6569961df09506625afe
Raymond Healy <rmh2805@g.rit.edu>
2020-04-02 13:17:17 -0400

    Added word count option, prompt user for type of query and parameters

--- wikiQueries.py
+++ wikiQueries.py
@@ -60,21 +60,57 @@ def getText(data):
 
 
 def substringFrequency(text, substring):
-    text = ' ' + text.strip() + ' '  # Buffer start and end of line to catch matches at the beginning and end of line
-    return float(text.count(substring) * len(substring))/float(len(text))
+    text = ' ' + text.strip().lower() + ' '
+    return float(text.count(substring))/float(len(text) * len(substring))
 
 
-nTrials = 100
-sStrs = ['aa', 'ee', 'ii', 'oo', 'uu']
-for sStr in sStrs:
-    nlSum = 0.0
-    enSum = 0.0
-    for i in range(0, nTrials):
-        nlSum += substringFrequency(getText(getPage(nlRand)), sStr)
-        enSum += substringFrequency(getText(getPage(enRand)), sStr)
+def wordLen(text):
+    text = text.strip().split()
+    numWords = 0
+    textSum = 0
+    for word in text:
+        if len(word) == 0:
+            continue
+        numWords += 1
+        textSum += len(word)
 
-    print('\nFrequencies for substring "' + sStr + '"')
-    print('\tnl: ' + str(nlSum/nTrials))
-    print('\ten: ' + str(enSum/nTrials))
+    return float(textSum)/float(numWords)
+
+def main():
+    print('====<WikiQuery>====')
+
+    while True:
+        stIn = ''
+        while len(stIn) == 0 or (not stIn[0] == 'w' and not stIn[0] == 's' and not stIn[0] == 'q'):
+            stIn = input('\n\tWord Count (w) or Substring (s) (quit is \'q\'): ').strip().lower()
+        nTrials = ''
+        if stIn[0] == 'q':
+            break
+
+        while len(nTrials) == 0 or not nTrials.isnumeric():
+            nTrials = input('\tHow many trials to run: ').strip().lower()
+        nTrials = int(nTrials)
+
+        enSum = 0.0
+        nlSum = 0.0
+        if stIn[0] == 'w':
+            for i in range(0, nTrials):
+                enSum += wordLen(getText(getPage(enRand)))
+                nlSum += wordLen(getText(getPage(nlRand)))
+
+            print('\n\tAvg word lengths:')
+
+        if stIn[0] == 's':
+            sStr = input('\tEnter the substring to scan for: ').strip().lower()
+            for i in range(0, nTrials):
+                nlSum += substringFrequency(getText(getPage(nlRand)), sStr)
+                enSum += substringFrequency(getText(getPage(enRand)), sStr)
+
+            print('\n\tFrequencies for substring "' + sStr + '"')
+
+        print('\t\ten: ' + str(enSum / float(nTrials)))
+        print('\t\tnl: ' + str(nlSum / float(nTrials)))
 
 
+if __name__ == '__main__':
+    main()
